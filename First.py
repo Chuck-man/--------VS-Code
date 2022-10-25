388abf5e992b43578dbe35eb9a06e9243cd440d2
Chuck-man <sgold2398@gmail.com>
2022-10-25 21:40:33 +0400

    Создание директории

--- First.py
+++ First.py
@@ -1,1 +1,18 @@
 #Лабораторная работа 1
+import re
+from bs4 import BeautifulSoup
+import requests
+import os
+import time
+import cv2
+
+def scraping(url):
+
+    if not os.path.isdir("dataset"): 
+        os.mkdir("dataset")
+    if not os.path.isdir("dataset" + url): 
+        os.mkdir("dataset"+ url)
+    url_full = "https://yandex.ru/images/search?text=" + url
+
+    array = []
+    
