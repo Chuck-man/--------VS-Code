c4e56d321f56785769bf0b8357cb61e28201ec6a
Chuck-man <sgold2398@gmail.com>
2022-11-22 22:16:07 +0400

    Доработка итератора

--- Second/Iterator.py
+++ Second/Iterator.py
@@ -6,20 +6,23 @@ class Iterator:
         self.file = file
         self.score = 0
         self.data = []
-        with open(self.file_name) as reading_file:
+        with open(self.file) as reading_file:
             file_reader = csv.reader(reading_file, delimiter=";")
             for i in file_reader:
                 self.data.append(i[0])
-            self.limit = len(self.data)
+            self.full = len(self.data)
     
     def __iter__(self):
         return self
     
     def __next__(self):
-        if self.counter < self.limit:
-            i = self.counter
-            self.counter += 1
+        if self.score < self.full:
+            i = self.score
+            self.score += 1
             return self.data[i]
         else:
             raise StopIteration
 
+instance = Iterator("cat", "dataset_csv.csv")
+print(next(instance))
+print(next(instance))
