dbf337c5a3171362fe9508254d2e2fb60e2e5a0a
Chuck-man <sgold2398@gmail.com>
2022-11-22 22:01:33 +0400

    Корректировка функции

--- Second/Random_copying.py
+++ Second/Random_copying.py
@@ -5,8 +5,8 @@ import csv
 
 def Randomizer(path: str, path_copier: str):
 
-    if not os.path.exists("path_copier"):
-        os.mkdir("path_copier")
+    if not os.path.exists("dataset_another"):
+        os.mkdir("dataset_another")
 
     source = os.listdir(path + "/")
     data = []
@@ -14,9 +14,14 @@ def Randomizer(path: str, path_copier: str):
     for i in source:
         source_data = os.listdir(path + "/" + i)
         for j in source_data:
-            rand = random.randint(0, 10000)
-            value = str(rand)
-            shutil.copyfileobj(os.path.join(path, i + "/" + j), os.path.join(path_copier + "/", value + ".jpg"))
+            exist = True
+            
+            while(exist):
+                rand = random.randint(0, 10000)
+                value = str(rand)
+                exist = os.path.exists(path_copier + "/" + value + ".jpg")
+                
+            shutil.copy(os.path.join(path + "/", i + "/" + j), os.path.join(path_copier + "/", value + ".jpg"))
             absolute = os.path.abspath(path + "/" + i + "/" + j)
             regarding = os.path.relpath(path + "/" + i + "/" + j)
             data.append([absolute, regarding, i])
@@ -25,4 +30,4 @@ def Randomizer(path: str, path_copier: str):
         writer = csv.writer(file, delimiter=";")
         writer.writerows(data)
 
-Randomizer("dataset/")
+Randomizer("dataset", "dataset_another")
