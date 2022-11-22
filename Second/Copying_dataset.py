2e76a210f9ca6f05b239754366d514cc03fd9c14
Chuck-man <sgold2398@gmail.com>
2022-11-22 22:07:38 +0400

    Корректировка фукнции и создание файла-аннотации

--- Second/Copying_dataset.py
+++ Second/Copying_dataset.py
@@ -1,14 +1,26 @@
 import os
 import shutil
+import csv
 
 def copy(path: str, path_copier: str):
 
-    if not os.path.exists("path_copier"):
-        os.mkdir("path_copier")
+    if not os.path.exists("dataset_copier"):
+        os.mkdir("dataset_copier")
 
     source = os.listdir(path + "/")
+    data = []
 
     for i in source:
         source_data = os.listdir(path + "/" + i)
+
         for j in source_data:
-            shutil.copyfileobj(os.path.join(path + "/" + i, j), os.path.join(path_copier + "/", i + "__" + j))
+            shutil.copy(os.path.join(path + "/" + i, j), os.path.join(path_copier + "/", i + "__" + j))
+            absolute = os.path.abspath(path + "/" + i + "/" + j)
+            regarding = os.path.relpath(path + "/" + i + "/" + j)
+            data.append([absolute, regarding, i])
+
+    with open(path_copier + ".csv", "w", newline="") as file:
+        writer = csv.writer(file, delimiter=";")
+        writer.writerows(data)
+
+copy("dataset", "dataset_copier")
