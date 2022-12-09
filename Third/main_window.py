f5c2e7d39a908139ddcd81ba5f5ffc378cdc4f0c
Chuck-man <sgold2398@gmail.com>
2022-12-09 21:21:52 +0400

    Создание и подключение библиотек

---
+++ Third/main_window.py
@@ -0,0 +1,21 @@
+import re
+from scraping import scraping
+from create_csv import creating_csv
+from copying_dataset import copying_dataset
+from random_copying import random_copying
+from iterator import Iterator
+from PyQt5.QtWidgets import (
+    QWidget,
+    QPushButton,
+    QHBoxLayout,
+    QVBoxLayout,
+    QApplication,
+    QDesktopWidget,
+    QLineEdit,
+    QInputDialog,
+    QLabel,
+    QMainWindow,
+    QFileDialog
+)
+from PyQt5.QtGui import QPixmap
+
