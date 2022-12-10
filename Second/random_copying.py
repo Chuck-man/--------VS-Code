aa1825726e77ffec845f0d36da9a1406a1febcd4
Chuck-man <sgold2398@gmail.com>
2022-12-10 21:11:38 +0400

    Создание класса

--- Second/random_copying.py
+++ Second/random_copying.py
@@ -21,9 +21,9 @@ def random_copying(path: str, path_copier: str):
             while(exist):
                 rand = random.randint(0, 10000)
                 value = str(rand)
-                exist = os.path.exists(path_copier + "/" + value + ".jpg")
-                
-            shutil.copy(os.path.join(path + "/", i + "/" + j), os.path.join(path_copier + "/", value + ".jpg"))
+                exist = os.path.exists(path_copier + "/" + value + ".jpg")    
+                shutil.copy(os.path.join(path + "/", i + "/" + j), os.path.join(path_copier + "/", value + ".jpg"))
+            
             absolute = os.path.abspath(path + "/" + i + "/" + j)
             regarding = os.path.relpath(path + "/" + i + "/" + j)
             data.append([absolute, regarding, i])
